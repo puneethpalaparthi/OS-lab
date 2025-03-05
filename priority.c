6832485e40bf052ac95c0c38b558d8d3cdf4af9c
Puneeth <160226241+puneethpalaparthi@users.noreply.github.com>
2025-03-05 11:56:41 +0530

    Update priority.c

--- priority.c
+++ priority.c
@@ -1,45 +1,37 @@
-#include<stdio.h>
-main()
-{
-int p[20],bt[20],pri[20], wt[20],tat[20],i, k, n, temp;
-float wtavg, tatavg;
-clrscr();
-printf("Enter the number of processes --- ");
+#include <stdio.h>
+int main(){
+int p[10],pri[10],bt[10],wt[10],tat[10],i,n,temp,j;
+float avgwt,avgtat;
+printf("enter no.process");
 scanf("%d",&n);
-for(i=0;i<n;i++)
-{
-p[i] = i;
-printf("Enter the Burst Time & Priority of Process %d --- ",i);
-scanf("%d %d",&bt[i], &pri[i]);
+for(i=0;i<n;i++){
+p[i]=i;
+printf("enter busrt and priority %d",i);
+scanf("%d%d",&bt[i],&pri[i]);
 }
-for(i=0;i<n;i++)
-for(k=i+1;k<n;k++)
-if(pri[i] > pri[k])
-{
+for(i=0;i<n;i++){
+for(j=i+1;j<n;j++){
+if(pri[i]>pri[j]){
 temp=p[i];
-p[i]=p[k];
-p[k]=temp;
+p[j]=p[i];
+p[j]=temp;
 temp=bt[i];
-bt[i]=bt[k];
-bt[k]=temp;
-temp=pri[i];
-pri[i]=pri[k];
-pri[k]=temp;
+bt[i]=bt[j];
+bt[j]=temp;
+}}}
+avgwt=wt[0]=0;
+avgtat=tat[0]=bt[0];
+for(i=1;i<n;i++){
+wt[i]=wt[i-1]+bt[i-1];
+tat[i]=tat[i-1]+bt[i];
+avgwt+=wt[i];
+avgtat+=tat[i];
 }
-wtavg = wt[0] = 0;
-tatavg = tat[0] = bt[0];
-for(i=1;i<n;i++)
-{
-wt[i] = wt[i-1] + bt[i-1];
-tat[i] = tat[i-1] + bt[i];
-wtavg = wtavg + wt[i];
-tatavg = tatavg + tat[i];
+printf("\nprocess\t\tpriority\t\tburst\t\twaiting\t\tturnaround");
+for(i=0;i<n;i++){
+printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t",p[i],pri[i],bt[i],wt[i],tat[i]);
 }
-printf("\nPROCESS\t\tPRIORITY\tBURST TIME\tWAITING 
-TIME\tTURNAROUND TIME");
-for(i=0;i<n;i++)
-printf("\n%d \t\t %d \t\t %d \t\t %d \t\t %d ",p[i],pri[i],bt[i],wt[i],tat[i]);
-printf("\nAverage Waiting Time is --- %f",wtavg/n);
-printf("\nAverage Turnaround Time is --- %f",tatavg/n);
-getch();
+printf("\nAverage Waiting time:%f\n",avgwt/n);
+printf("Average Turn Around Time%f\n",avgtat/n);
+return 0;
 }
